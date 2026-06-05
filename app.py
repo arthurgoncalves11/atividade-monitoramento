59bc9556bfdd7293a15975c22ced46333d6739a9
Arthur Santos Gonçalves <arthursantosgoncalves587@gmail.com>
2026-06-05 12:35:01 -0300

    ajuste

--- app.py
+++ app.py
@@ -6,23 +6,31 @@ app = Flask(__name__)
 @app.route("/evidencia/<id>")
 def evidencia(id):
 
-    ip = request.remote_addr
+    ip = request.headers.get(
+        "X-Forwarded-For",
+        request.remote_addr
+    )
+
     user_agent = request.headers.get("User-Agent")
-    data = datetime.now()
 
     log = f"""
-    Evidência: {id}
-    Data/Hora: {data}
-    IP: {ip}
-    User-Agent: {user_agent}
-    ------------------------
-    """
-
-    print(log)
+Evidência: {id}
+Data/Hora: {datetime.now()}
+IP: {ip}
+User-Agent: {user_agent}
+------------------------
+"""
 
     with open("logs.txt", "a", encoding="utf-8") as arquivo:
         arquivo.write(log)
 
-    return "<h1>Evidência acessada!</h1>"
+    return f"""
+    <h1>Evidência acessada!</h1>
 
-app.run(host="0.0.0.0", port=5000)
+    <pre>
+{log}
+    </pre>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
